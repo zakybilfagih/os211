8c6191c285d57ae183bd03085bf25275fe715a2a
mackarelfish <mackarelfish93@gmail.com>
2021-03-23 23:47:09 +0700

    finished w03

--- README.md
+++ README.md
@@ -3,3 +3,4 @@ Operating Systems 2021-1 (Semester Genap 2020/2021)
 
 [W01](https://mackarelfish.github.io/os211/W01)
 [W02](https://mackarelfish.github.io/os211/W02)
+[W02](https://mackarelfish.github.io/os211/W03)
--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -21,25 +21,25 @@ REC1="operatingsystems@vlsm.org"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
 
-# [ -d $HOME/RESULT ] || mkdir -p $HOME/RESULT
-# pushd $HOME/RESULT
-# for II in W?? ; do
-#     [ -d $II ] || continue
-#     TARFILE=my$II.tar.bz2
-#     TARFASC=$TARFILE.asc
-#     rm -f $TARFILE $TARFASC
-#     echo "tar cfj $TARFILE $II/"
-#     tar cfj $TARFILE $II/
-#     echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
-#     gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
-# done
-# popd
-# 
-# rm -f $HOME/RESULT/fakeDODOL
-# for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
-#    echo "Check and move $II..."
-#    [ -f $II ] && mv -f $II .
-# done
+[ -d $HOME/RESULT ] || mkdir -p $HOME/RESULT
+pushd $HOME/RESULT
+for II in W?? ; do
+    [ -d $II ] || continue
+    TARFILE=my$II.tar.bz2
+    TARFASC=$TARFILE.asc
+    rm -f $TARFILE $TARFASC
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
+    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
+
+rm -f $HOME/RESULT/fakeDODOL
+for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
+   echo "Check and move $II..."
+   [ -f $II ] && mv -f $II .
+done
 
 echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
